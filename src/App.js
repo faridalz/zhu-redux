07bfe3cbb53cb14daae4c0b1df4d28a5f2af2cb6
unknown <feritalyv@gmail.com>
2022-10-22 19:48:29 +0200

    resolved router issues

--- src/App.js
+++ src/App.js
@@ -4,18 +4,14 @@ import Music from "./components/music";
 import Gallery from "./components/gallery";
 import GalleryOther from "./components/galleryOther";
 import Videos from "./components/videos";
-import Footer from "./components/footer";
-import Cart from "./components/cart";
+
 import './App.css';
-import Navbar from "./components/navbar";
 
 
 
 const App = () => {
   return (
     <>
-        < Navbar />
-        < Cart />
         <Routes>
           <Route path= '/' element={< MainPage />} />
           <Route path= 'music' element={<Music />} />
@@ -23,7 +19,6 @@ const App = () => {
           <Route path= 'gallery-other' element={<GalleryOther />} />
           <Route path= 'videos' element={<Videos />} />
         </Routes>
-        < Footer />
     </>    
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import App from './App';
 import {Provider} from "react-redux";
 import { store } from './redux/store';
 import {BrowserRouter as Router} from 'react-router-dom';
+import Navbar from './components/navbar';
+import Cart from './components/cart';
+import Footer from './components/footer';
 
 
 {/* <Router basename={ process.env.PUBLIC_URL + '/' }>
@@ -14,7 +17,10 @@ ReactDOM.render(
   <React.StrictMode>
       <Provider store={store}>
         <Router>
+          < Navbar />
+          < Cart />
           <App />
+          < Footer />
         </Router>
       </Provider>
   </React.StrictMode>,
